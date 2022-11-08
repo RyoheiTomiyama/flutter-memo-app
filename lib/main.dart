90897fcc5d8241e02210129ea4a132135ef96460
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2022-11-08 19:23:55 +0900

    Riverpodを使ったコードに書き換える

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
 
 class MyApp extends HookWidget {
@@ -21,17 +24,15 @@ class MyApp extends HookWidget {
   }
 }
 
-class MyHomePage extends HookWidget {
+class MyHomePage extends HookConsumerWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  Widget build(BuildContext context) {
-    final _counter = useState(0);
-    void _incrementCounter() {
-      _counter.value++;
-    }
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
+    final counterNotifier = ref.watch(counterProvider.notifier);
 
     return Scaffold(
       appBar: AppBar(
@@ -45,17 +46,36 @@ class MyHomePage extends HookWidget {
               'You have pushed the button this many times:',
             ),
             Text(
-              '${_counter.value}',
+              '${counter.count}',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: counterNotifier.incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+@immutable
+class Counter {
+  final int count;
+  const Counter({
+    required this.count,
+  });
+}
+
+class CounterModel extends StateNotifier<Counter> {
+  CounterModel() : super(const Counter(count: 0));
+
+  incrementCounter() {
+    state = Counter(count: state.count + 1);
+  }
+}
+
+final counterProvider =
+    StateNotifierProvider<CounterModel, Counter>((ref) => CounterModel());
