54e05bd8bd60aca51d97d9ac8396bc428bbc0b29
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2022-11-09 10:38:41 +0900

    add SuperEditor Widget

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:memo_app/provider/counter_provider.dart';
+import 'package:super_editor/super_editor.dart';
 
 class Home extends HookConsumerWidget {
   const Home({super.key, required this.title});
@@ -9,8 +9,8 @@ class Home extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterProvider);
-    final counterNotifier = ref.watch(counterProvider.notifier);
+    final _doc = _createInitialDocument();
+    final _docEditor = DocumentEditor(document: _doc as MutableDocument);
 
     return Scaffold(
       appBar: AppBar(
@@ -25,25 +25,118 @@ class Home extends HookConsumerWidget {
           ),
         ],
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '${counter.count}',
-              style: Theme.of(context).textTheme.headline4,
+          children: [
+            SuperEditor(
+              editor: _docEditor,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counterNotifier.incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: counterNotifier.incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Document _createInitialDocument() {
+  return MutableDocument(
+    nodes: [
+      ImageNode(
+        id: DocumentEditor.createNodeId(),
+        imageUrl: 'https://i.imgur.com/fSZwM7G.jpg',
+      ),
+      ParagraphNode(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'Example Document',
+        ),
+        metadata: {
+          'blockType': header1Attribution,
+        },
+      ),
+      HorizontalRuleNode(id: DocumentEditor.createNodeId()),
+      ParagraphNode(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed sagittis urna. Aenean mattis ante justo, quis sollicitudin metus interdum id. Aenean ornare urna ac enim consequat mollis. In aliquet convallis efficitur. Phasellus convallis purus in fringilla scelerisque. Ut ac orci a turpis egestas lobortis. Morbi aliquam dapibus sem, vitae sodales arcu ultrices eu. Duis vulputate mauris quam, eleifend pulvinar quam blandit eget.',
+        ),
+      ),
+      ListItemNode.unordered(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'This is an unordered list item',
+        ),
+      ),
+      ListItemNode.unordered(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'This is another list item',
+        ),
+      ),
+      ListItemNode.unordered(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+            text: 'This is a 3rd list item, with a link',
+            spans: AttributedSpans(
+              attributions: [
+                SpanMarker(
+                    attribution:
+                        LinkAttribution(url: Uri.https('example.org', '')),
+                    offset: 30,
+                    markerType: SpanMarkerType.start),
+                SpanMarker(
+                    attribution:
+                        LinkAttribution(url: Uri.https('example.org', '')),
+                    offset: 35,
+                    markerType: SpanMarkerType.end),
+              ],
+            )),
+      ),
+      ParagraphNode(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+            text:
+                'Cras vitae sodales nisi. Vivamus dignissim vel purus vel aliquet. Sed viverra diam vel nisi rhoncus pharetra. Donec gravida ut ligula euismod pharetra. Etiam sed urna scelerisque, efficitur mauris vel, semper arcu. Nullam sed vehicula sapien. Donec id tellus volutpat, eleifend nulla eget, rutrum mauris.'),
+      ),
+      ListItemNode.ordered(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'First thing to do',
+        ),
+      ),
+      ListItemNode.ordered(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'Second thing to do',
+        ),
+      ),
+      ListItemNode.ordered(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text: 'Third thing to do',
+        ),
+      ),
+      ParagraphNode(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text:
+              'Nam hendrerit vitae elit ut placerat. Maecenas nec congue neque. Fusce eget tortor pulvinar, cursus neque vitae, sagittis lectus. Duis mollis libero eu scelerisque ullamcorper. Pellentesque eleifend arcu nec augue molestie, at iaculis dui rutrum. Etiam lobortis magna at magna pellentesque ornare. Sed accumsan, libero vel porta molestie, tortor lorem eleifend ante, at egestas leo felis sed nunc. Quisque mi neque, molestie vel dolor a, eleifend tempor odio.',
+        ),
+      ),
+      ParagraphNode(
+        id: DocumentEditor.createNodeId(),
+        text: AttributedText(
+          text:
+              'Etiam id lacus interdum, efficitur ex convallis, accumsan ipsum. Integer faucibus mollis mauris, a suscipit ante mollis vitae. Fusce justo metus, congue non lectus ac, luctus rhoncus tellus. Phasellus vitae fermentum orci, sit amet sodales orci. Fusce at ante iaculis nunc aliquet pharetra. Nam placerat, nisl in gravida lacinia, nisl nibh feugiat nunc, in sagittis nisl sapien nec arcu. Nunc gravida faucibus massa, sit amet accumsan dolor feugiat in. Mauris ut elementum leo.',
+        ),
+      ),
+    ],
+  );
 }
