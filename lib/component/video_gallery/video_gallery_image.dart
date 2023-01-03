5af23adefd706efa8202222a76be6de11df54a7f
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2023-01-03 22:45:15 +0900

    try future cancelable

--- lib/component/video_gallery/video_gallery_image.dart
+++ lib/component/video_gallery/video_gallery_image.dart
@@ -1,3 +1,4 @@
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,9 +13,22 @@ class VideoGalleryImage extends HookConsumerWidget {
   Widget build(BuildContext context, ref) {
     final videoGalleryNotifier = ref.watch(videoGalleryProvider.notifier);
 
+    final cancelableOperation = useState<CancelableOperation?>(null);
+
     final getThumbnail = useMemoized(() {
       return videoGalleryNotifier.getThumbnail(gallery);
     }, [gallery]);
+
+    useEffect(() {
+      cancelableOperation.value = CancelableOperation.fromFuture(getThumbnail);
+      return null;
+    }, [getThumbnail]);
+
+    useEffect(() {
+      return () {
+        cancelableOperation.value?.cancel();
+      };
+    }, []);
 
     final snapshot = useFuture(getThumbnail);
 
